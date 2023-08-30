a5b8e377d9699bb1cbef12e2eaa518133f0b67d7
positiview <goaud2022@gmail.com>
2023-08-30 10:11:03 +0900

    the main commit test

---
+++ src/main/java/com/example/test230830/Test230830Application.java
@@ -0,0 +1,13 @@
+package com.example.test230830;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test230830Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test230830Application.class, args);
+    }
+
+}
